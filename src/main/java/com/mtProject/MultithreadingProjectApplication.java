0aa4cf974444cac7b2c0a4defdc6eb0fcccfdf0e
Naresh-Kumar-Kotha98 <targetnareshk@gmail.com>
2025-10-20 20:02:31 +0530

    first commit

---
+++ src/main/java/com/mtProject/MultithreadingProjectApplication.java
@@ -0,0 +1,13 @@
+package com.mtProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultithreadingProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultithreadingProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mtProject/MultithreadingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultithreadingProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
